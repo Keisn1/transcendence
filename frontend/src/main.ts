62e696168543e87c6c6abba961ef51f9b1563d8c
Kay Freyer <kfreyer@c2r1s1.42wolfsburg.de>
2025-07-09 17:14:13 +0200

    take a step towards Client-side routing

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,5 +2,30 @@ import "./style.css";
 import "./templates/navbar/navbar.ts"; // Add this line
 import "./templates/adLightbox/adLightbox.ts"; // Add this line
 
+const router = () => {
+  let routes = [
+    { path: "/", view: "Dashboard" },
+    { path: "/game", view: "Game" },
+  ];
+
+  let potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      result: location.pathname.match(route.path),
+    };
+  });
+
+  let match = potentialMatches.find(
+    (potentialMatch) => potentialMatch.result !== null,
+  );
+
+  if (!match) {
+    match = {
+      route: routes[0],
+      result: [location.pathname],
+    };
+  }
+};
+
 document.querySelector<HTMLAnchorElement>("#link-2")!.href =
   "/src/game/game.html";
