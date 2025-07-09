c0138b23583221e417052046d32996d0508dda70
Kay Freyer <kfreyer@c2r1s1.42wolfsburg.de>
2025-07-09 16:55:03 +0200

    refactor adLightbox/Navbar into templates

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,55 +1,6 @@
 import "./style.css";
-import navbarTemplate from "./templates/navbar.html?raw";
-import adLightboxTemplate from "./templates/adLightbox.html?raw";
-
-document.querySelector<HTMLDivElement>("#navbar-container")!.innerHTML =
-  navbarTemplate;
-
-document.querySelector<HTMLDivElement>("#ad-lightbox")!.innerHTML =
-  adLightboxTemplate;
+import "./templates/navbar/navbar.ts"; // Add this line
+import "./templates/adLightbox/adLightbox.ts"; // Add this line
 
 document.querySelector<HTMLAnchorElement>("#link-2")!.href =
   "/src/game/game.html";
-
-// menu
-const userMenuButton = document.getElementById("user-menu-button")!;
-const menu = document.querySelector('[role="menu"]')!;
-
-document.addEventListener("click", (e) => {
-  const target = e.target as HTMLElement;
-
-  if (userMenuButton.contains(target)) {
-    menu.classList.toggle("hidden");
-    return;
-  }
-
-  if (!menu.contains(target)) {
-    menu.classList.add("hidden");
-  }
-});
-
-// advertisement
-const adLightbox = document.getElementById("ad-lightbox")!;
-const adCloseButton = document.getElementById("ad-close-btn")!;
-
-function showAd(): void {
-  adLightbox.classList.remove("hidden");
-}
-
-function hideAd(): void {
-  adLightbox.classList.add("hidden");
-}
-
-adCloseButton.addEventListener("click", hideAd);
-
-adLightbox.addEventListener("click", (e) => {
-  const target = e.target as HTMLElement;
-  if (target === adLightbox) {
-    hideAd();
-  }
-});
-
-window.addEventListener("load", () => {
-  console.log("hello");
-  setTimeout(showAd, 2000); // wait 2 seconds
-});
---
+++ frontend/src/templates/navbar/navbar.ts
@@ -0,0 +1,21 @@
+// menu
+import navbarTemplate from "./navbar.html?raw";
+
+document.querySelector<HTMLDivElement>("#navbar-container")!.innerHTML =
+    navbarTemplate;
+
+const userMenuButton = document.getElementById("user-menu-button")!;
+const menu = document.querySelector('[role="menu"]')!;
+
+document.addEventListener("click", (e) => {
+    const target = e.target as HTMLElement;
+
+    if (userMenuButton.contains(target)) {
+        menu.classList.toggle("hidden");
+        return;
+    }
+
+    if (!menu.contains(target)) {
+        menu.classList.add("hidden");
+    }
+});
