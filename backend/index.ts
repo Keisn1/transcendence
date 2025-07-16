a190f9dad821f210d4e1c17a719ef3fea2a144d7
obluda2173 <obluda2173@gmail.com>
2025-07-16 16:09:50 +0200

    working on backend

--- backend/index.ts
+++ backend/index.ts
@@ -11,28 +11,65 @@ server.get("/api/profile", async (request, reply) => {
     return "your profile\n";
 });
 
-server.get("/login/:user", async (request: FastifyRequest<{ Params: { user: string } }>, reply) => {
-    let user = request.params.user;
+type User = {
+    id: number,
+    username: string,
+    email: string,
+    avatar: string,
+}
 
-    let obj1 = {
-        id: "123",
-        username: "john_doe",
-        email: "john@example.com",
-        avatar: "https://example.com/avatar.jpg",
-    };
-    let obj2 = {
-        id: "124",
-        username: "jane_doe",
-        email: "jane@example.com",
-        avatar: "https://example.com/avatar.jpg",
-    };
+type CreateUserBody = {
+    username: string;
+    email: string;
+    avatar: string;
+}
 
-    if (user == "john_doe") {
-        return JSON.stringify(obj1);
-    } else if (user == "jane_doe") {
-        return JSON.stringify(obj2);
+var userDb: Record< number, User > = {
+        123: {
+            id: 123,
+            username: "john_doe",
+            email: "john@example.com",
+            avatar: "https://example.com/avatar.jpg",
+        },
+        124: {
+            id: 124,
+            username: "jane_doe",
+            email: "jane@example.com",
+            avatar: "https://example.com/avatar.jpg",
+        }
+}
+
+server.get("/api/user/:id", async (request: FastifyRequest<{ Params: { id: number } }>, reply) => {
+    let userId = request.params.id;    
+
+    if (!(userId in userDb)) {
+        return reply.code(404).send({ error: 'User not found' });
     }
+    return userDb[userId];
 });
+
+server.post("/api/user", async (request: FastifyRequest<{ Body: CreateUserBody }>, reply) => {
+    let newUserData = request.body;
+
+    if (newUserData.username in userDb) {
+        return reply.code(400).send({ error: 'Username is already being used' });
+    }
+
+    const keys = Object.keys(userDb);
+    const lastKey = keys[keys.length - 1];
+    const newUserId: number = +lastKey + 1;
+
+    let newUser: User;
+    newUser = { 
+        id: newUserId,
+        username: newUserData.username, 
+        email: newUserData.email,
+        avatar: newUserData.avatar
+    };
+
+    userDb[newUserId] = newUser;
+})
+
 
 server.listen({ port: 3000 }, (err, address) => {
     if (err) {
