07d9b33ee9532e29f130fdfea3208c2b7d55e274
obluda2173 <obluda2173@gmail.com>
2025-07-16 15:09:26 +0200

    just push

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import fastify from "fastify";
+import { FastifyRequest } from "fastify";
 
 const server = fastify();
 
@@ -8,6 +9,29 @@ server.get("/ping", async (request, reply) => {
 
 server.get("/api/profile", async (request, reply) => {
     return "your profile\n";
+});
+
+server.get("/login/:user", async (request: FastifyRequest<{ Params: { user: string } }>, reply) => {
+    let user = request.params.user;
+
+    let obj1 = {
+        id: "123",
+        username: "john_doe",
+        email: "john@example.com",
+        avatar: "https://example.com/avatar.jpg",
+    };
+    let obj2 = {
+        id: "124",
+        username: "jane_doe",
+        email: "jane@example.com",
+        avatar: "https://example.com/avatar.jpg",
+    };
+
+    if (user == "john_doe") {
+        return JSON.stringify(obj1);
+    } else if (user == "jane_doe") {
+        return JSON.stringify(obj2);
+    }
 });
 
 server.listen({ port: 3000 }, (err, address) => {
