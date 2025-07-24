5ecd54306eff994dd2acf880ca6e35fab5085673
obluda2173 <obluda2173@gmail.com>
2025-07-24 18:17:29 +0200

    making beauty

--- frontend/src/game/game.ts
+++ frontend/src/game/game.ts
@@ -35,6 +35,7 @@ export class PongGame {
     private timePassed: number = 1000;
     private feedFrequency: number = 1000;
     private aiController: AiController | null = null;
+    private gameFont = { large: "200px monospace", normal: "42px monospace",};
 
     constructor(canvas: HTMLCanvasElement, config: GameConfig = {}) {
         const defaultControls: ControlsConfig = {
@@ -74,6 +75,9 @@ export class PongGame {
             left: new Paddle("left", this.config.paddleConfig, canvas),
             right: new Paddle("right", this.config.paddleConfig, canvas),
         };
+
+        this.ctx.textAlign = "center";
+        this.ctx.textBaseline = "middle";
     }
 
     public setAiLevel(level: AiLevel) {
@@ -82,7 +86,7 @@ export class PongGame {
             this.aiController = null;
         }
         if (level !== "none") {
-            if (level == "easy") this.feedFrequency = 1500; // not the best solution
+            if (level == "easy") this.feedFrequency = 1300; // not the best solution
             this.setupAi();
         }
     }
@@ -121,7 +125,7 @@ export class PongGame {
     }
 
     private async startTimer() {
-        this.ctx.font = "200px serif";
+        this.ctx.font = this.gameFont.large;
         this.ctx.fillStyle = "#fff";
 
         for (let i = 3; i > 0; --i) {
@@ -132,8 +136,16 @@ export class PongGame {
     }
 
     private isGameOver(): boolean {
-        if (this.scores.player1 >= this.config.winningScore || this.scores.player2 >= this.config.winningScore) {
+        const p1Score = this.scores.player1;
+        const p2Score = this.scores.player2;
+        const winScore = this.config.winningScore;
+        let winMessage: string = p1Score > p2Score ? "player 1 wins" : "player 2 wins";
+
+        if (p1Score >= winScore || p2Score >= winScore) {
             this.destroy();
+            this.ctx.font = this.gameFont.normal;
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.fillText(winMessage, this.canvas.width / 2, this.canvas.height / 2);
             return true;
         }
         return false;
@@ -148,13 +160,9 @@ export class PongGame {
             this.ball.draw(this.ctx);
         }
 
-        this.ctx.font = "42px serif";
-        this.ctx.fillText(`${this.scores.player1}`, this.canvas.width / 4.2, this.canvas.height / 10);
-        this.ctx.fillText(
-            `${this.scores.player2}`,
-            this.canvas.width / 2 + this.canvas.width / 4.2,
-            this.canvas.height / 10,
-        );
+        this.ctx.font = this.gameFont.normal;
+        this.ctx.fillText(`${this.scores.player1}`, this.canvas.width / 4, this.canvas.height / 16);
+        this.ctx.fillText(`${this.scores.player2}`, (3 * this.canvas.width) / 4, this.canvas.height / 16);
     }
 
     private checkCollision(): Paddle | null {
