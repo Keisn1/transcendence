12c8e67f584955c51924c46db5d228a6a26bc66a
Kay Freyer <kay.freyer@icloud.com>
2025-07-24 12:40:22 +0200

    delete old userService (Erik)

--- backend/services/user/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { getUser, createUser } from "../controllers/user.controller";
-import { ping, profile } from "../controllers/root.controller";
-
-export async function routes(app: FastifyInstance) {
-    // Stupid routes
-    app.get("/ping", ping);
-    app.get("/profile", profile);
-}
