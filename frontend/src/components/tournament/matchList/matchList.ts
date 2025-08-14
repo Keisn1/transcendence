b2b6e00b827c523461b5e75ef780fbac19aea59e
obluda2173 <obluda2173@gmail.com>
2025-08-14 16:43:50 +0200

    Revert "added user links in nextMatch"
    This reverts commit 00b2f9736c871530b3fdab0fda199df0d9878eda.

--- frontend/src/components/tournament/matchList/matchList.ts
+++ frontend/src/components/tournament/matchList/matchList.ts
@@ -1,6 +1,5 @@
 import { BaseComponent } from "../../BaseComponent.ts";
 import { TournamentController } from "../../../controllers/tournament.controller.ts";
-import { UserController } from "../../../controllers/user.controller.ts";
 import matchListTemplate from "./matchList.html?raw";
 import matchTemplate from "./match.html?raw";
 
@@ -18,8 +17,6 @@ export class MatchList extends BaseComponent {
         this.matchList = this.container.querySelector("#matches-list")!;
 
         this.populateData();
-
-        this.addEventListenerWithCleanup(this.matchList, "click", this.onUserClick.bind(this));
     }
     private populateData() {
         if (this.defaultComponents) this.fillMatchListDefault();
@@ -29,13 +26,9 @@ export class MatchList extends BaseComponent {
     private fillMatchList() {
         this.tournamentController.getTournament()!.matches.forEach((m) => {
             const status: string = `${m.result?.player1Score} : ${m.result?.player2Score}`;
-
-            const player1Span = `<span class="cursor-pointer text-black underline-none hover:text-indigo-600" data-username="${m.player1.username}">${m.player1.username}</span>`;
-            const player2Span = `<span class="cursor-pointer text-black underline-none hover:text-indigo-600" data-username="${m.player2.username}">${m.player2.username}</span>`;
-
             const html = matchTemplate
-                .replace(/{{player1}}/g, player1Span)
-                .replace(/{{player2}}/g, player2Span)
+                .replace(/{{player1}}/g, m.player1.username)
+                .replace(/{{player2}}/g, m.player2.username)
                 .replace(/{{status}}/g, status);
             this.matchList.insertAdjacentHTML("beforeend", html);
         });
@@ -45,24 +38,12 @@ export class MatchList extends BaseComponent {
         console.log("fill Macht list default");
         this.tournamentController.getTournamentDefault()!.matches.forEach((m) => {
             const status: string = `${m.result?.player1Score} : ${m.result?.player2Score}`;
-
-            const player1Span = `<span class="cursor-pointer text-black underline-none hover:text-indigo-600" data-username="${m.player1.username}">${m.player1.username}</span>`;
-            const player2Span = `<span class="cursor-pointer text-black underline-none hover:text-indigo-600" data-username="${m.player2.username}">${m.player2.username}</span>`;
-
             const html = matchTemplate
-                .replace(/{{player1}}/g, player1Span)
-                .replace(/{{player2}}/g, player2Span)
+                .replace(/{{player1}}/g, m.player1.username)
+                .replace(/{{player2}}/g, m.player2.username)
                 .replace(/{{status}}/g, status);
             this.matchList.insertAdjacentHTML("beforeend", html);
         });
-    }
-
-    private onUserClick(e: Event) {
-        e.preventDefault();
-        const target = e.target as HTMLElement;
-        const username = target.dataset.username;
-        if (!username) return;
-        UserController.getInstance().navigateToUser(`/user/${encodeURIComponent(username)}`);
     }
 
     destroy(): void {
